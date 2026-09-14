9cf3ac20d5e198d7200961b09f5c7ee1b787b309
Khris H. <contact@devkhris.com>
2026-09-14 03:30:36 -0600

    feat: added dotenv

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from app import create_app
 
 app = create_app(os.environ.get("FLASK_ENV", "development"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=False)
